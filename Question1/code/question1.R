d9a07c49254fc9c11a6122732124e0a315f02c29
RiazPaleker <riaz2502@gmail.com>
2024-06-18 11:17:33 +0200

    importing code for Q1
    importing code for question 1

---
+++ Question1/code/question1.R
@@ -0,0 +1,8 @@
+library(tidyverse)
+Baby_Names <- readRDS("./data/US_Baby_names/Baby_Names_By_US_State.rds")
+charts <- readRDS("./data/US_Baby_names/charts.rds")
+HBO_Titles <- readRDS("./data/US_Baby_names/HBO_titles.rds")
+HBO_Credits <- readRDS("./data/US_Baby_names/HBO_credits.rds")
+
+
+
