b62188f69aab5c379308c806e0f502b49cef548f
RiazPaleker <riaz2502@gmail.com>
2024-06-18 17:12:16 +0200

    Update question1.R
    plot for charts

--- Question1/code/question1.R
+++ Question1/code/question1.R
@@ -9,6 +9,8 @@ HBO_Credits <- readRDS("./data/US_Baby_names/HBO_credits.rds")
 library(dplyr)
 library(stats)
 
+# PERSISTENCE IN POPULAR NAMES
+
 top_25_names <- Baby_Names %>%
     group_by(Year, Gender) %>%
     top_n(25, Count) %>%
@@ -67,8 +69,67 @@ library(ggplot2)
 
 corr_plot = decade_correlation %>% ggplot() +
     geom_line(aes(x = Decade, y = Average_Correlation, color = as.factor(Lag)))+
-    facet_wrap(~ Gender) + labs(title = "Average Serial Correlation of Popular Names by Decade",
+    facet_wrap(~ Gender) + labs(title = "Average Correlation of Popular Names by Decade",
                                               x = "Decade",
                                               y = "Average Correlation",
                                               color = "Subsequent Year")
 
+
+# CHARTS
+
+subset_baby_names <- function(Baby_Names) {
+    # Filter the data for the year 2014 and state New York
+    filter_names <- Baby_Names %>%
+        filter(Year>=2010 & Year<=2014, State == "NY", Count>=1000 )%>%
+        arrange(desc(Count))
+
+    return(filter_names[,-c(1,4,5)])
+}
+
+new_names =subset_baby_names(Baby_Names)
+
+subset_charts  <- function(charts){
+
+    filter_charts = charts %>%
+        filter(date>="2010-01-01" & date<="2014-12-31", charts$`peak-rank`==1) %>%
+        mutate(year = year(date)) %>%
+        select(-date) %>%
+        rename(date = year)
+
+
+    return(filter_charts[,-c(1,2,4,5,6)])
+
+
+}
+charts_new=subset_charts(charts)
+colnames(charts_new) =c("Name", "Year")
+
+# Load required package for fuzzy matching
+install.packages("stringdist")
+library(stringdist)
+
+# Step 1: Find approximate matches between new_names$Name and dataset2$Artist
+matching_names <- stringdist::amatch(new_names$Name, charts_new$Name, maxDist = 5)
+
+# Step 2: Filter Dataset 1 based on matching indices
+names_to_plot <- new_names$Name[!is.na(matching_names)]
+
+# Step 3: Aggregate counts across years for names in names_to_plot
+agg_data <- new_names %>%
+    filter(Name %in% names_to_plot) %>%
+    group_by(Name, Year) %>%
+    summarise(TotalCount = sum(Count))
+
+
+# Step 4: Create the bubble plot using ggplot2
+library(ggplot2)
+
+charts_names_plot = agg_data %>% ggplot() +
+    geom_point(aes(x = Name, y = Year, size = TotalCount), shape = 21, fill="blue", alpha = 0.6, ) +
+    guides(size = F)+
+    labs(title = "Popularity of Names in Hot 100 Charts",
+         x = "Names",
+         y = "Total Count",
+         size = "Total Count")
+
+charts_names_plot
